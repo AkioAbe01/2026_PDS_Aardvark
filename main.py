1ecfff8ab2c2dad64aa4f3c5e5e6681ed00de204
Akio-Abe01 <azin@itu.dk>
2026-03-26 17:21:04 +0100

    tested loader.py code in main.py with debug prints

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#------------------------was already created in the repo---------------------------
 def main(features_path, prediction_results_path, model_path, load_model):
     """
     Docstring for main
@@ -25,9 +26,66 @@ def main(features_path, prediction_results_path, model_path, load_model):
 
 
     # write test results to CSV.
+#----------------------------------------------------------------------------------
+
+from src.loader import (
+    load_metadata,
+    filter_missing_masks,
+    get_image_mask_pairs,
+    load_image,
+    load_mask
+)
+
+print("--- Testing loader.py ---")
+
+metadata = load_metadata()
+print("Total metadata rows: ", len(metadata))
+
+filtered_metadata = filter_missing_masks(metadata)
+print("After filtering masks: ", len(filtered_metadata))
+
+pairs = get_image_mask_pairs(filtered_metadata)
+print("Number of pairs: ", len(pairs))
 
 
+image_path, mask_path = pairs[0]
+print(f"First image path: {image_path}\nFirst mask path: {mask_path}")
 
+image = load_image(image_path)
+mask = load_mask(mask_path)
+print("Image shape:", image.shape)
+print("Mask shape:", mask.shape)
+
+print("---Done---")
+
+
+#How to activate feature using main.py and loader.py (a little cheatsheet)
+'''
+Lets say we have featureA.py that requires a mask
+
+from src.featureA import func_name
+
+metadata = load_metadata()
+filtered_metadata = filter_missing_masks(metadata)
+pairs = get_image_mask_pairs(filtered_metadata)
+
+feature_values = []
+
+for image_path, mask_path in pairs:
+    image = load_image(image_path)
+    mask = load_mask(mask_path)
+
+    value = func_name(mask)
+
+
+    feature_values.append(value)
+
+print(feature_values[:5])
+
+'''
+
+
+#------------------------was already created in the repo---------------------------
 if __name__ == "__main__":
     features_path = "./data/features.csv"
     prediction_results_path = "./result/predictions/predictions_MODEL.csv"
@@ -35,3 +93,4 @@ if __name__ == "__main__":
     load_model = False
 
     main(features_path, prediction_results_path,model_path,load_model)
+#----------------------------------------------------------------------------------
