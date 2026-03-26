641bf4ec75c53333ad73abff94d3b26226ad3895
Akio-Abe01 <azin@itu.dk>
2026-03-26 19:59:42 +0100

    updated loader with function docstrings

--- src/loader.py
+++ src/loader.py
@@ -2,26 +2,33 @@ import pandas as pd
 from skimage import io
 import os
 
-#function load the metadata
+
 def load_metadata():
+    """Loads the metadata"""
+
     data = pd.read_csv("data/metadata.csv")
     return data
 
-#should be in main.py   0
-metadata = load_metadata()
+
 
 def build_image_path(row):
+    """ Builds a correct path for the image"""
+
     image_id = row["img_id"]
     path = "data/imgs/" + image_id
     return path
 
 def build_mask_path(row):
-    image_id = row["img_id"] #eg: PAT_1516_1765_530.png
+    """builds a correct path for the mask"""
+
+    image_id = row["img_id"]
     path = "data/masks/" + image_id[:-4] + "_mask.png" 
     return path
 
-#create a new df that will not include images that have no masks    
+   
 def filter_missing_masks(metadata):
+    """ Creates a new df that will not include images that have no masks and masks, that are not applied to any image in the metadata"""
+
     mask_files = set(os.listdir("data/masks")) #set of masks
     mask = metadata.apply(lambda row: (row["img_id"][:-4] + "_mask.png") in mask_files, axis=1) #check if expected mask is in reality in the given data
     filtered_metadata = metadata[mask]
@@ -33,17 +40,23 @@ def filter_multiple_lesions(path):
     #to be implemented
     return
 
-#load image and mask
+
 def load_image(path):
+    """ Loads image"""
+
     image = io.imread(path)
     return image
 
 def load_mask(path):
+    """Loads a mask"""
+
     mask = io.imread(path)
     return mask
 
-#get the mask_image pair and ensure they come from the same lesion
+
 def get_image_mask_pairs(metadata):
+    """ Gets the mask-image pair and ensures they come from the same lesion"""
+    
     pairs = []
     for _, row in metadata.iterrows():
         image_path = build_image_path(row)
